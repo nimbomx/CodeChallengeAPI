4bb1756b507c05f3dcc729f602029c5ca7cc607e
Ernesto Hernández <ernesto@nimbo.mx>
2019-10-04 09:36:44 -0500

    Test protocols functions

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,18 @@ use Illuminate\Http\Request;
 |
 */
 
+Route::get('/test', function (Request $request) {
+    return ['mesage' => 'getOk'];
+});
+Route::post('/test', function (Request $request) {
+    return ['mesage' => 'postOk'];
+});
+Route::put('/test', function (Request $request) {
+    return ['mesage' => 'putOk'];
+});
+Route::delete('/test', function (Request $request) {
+    return ['mesage' => 'deleteOk'];
+});
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
